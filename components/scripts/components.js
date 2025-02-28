21f6a35963cdb5e2742761a892b802b04811e392
joosthel <146737859+joosthel@users.noreply.github.com>
2025-02-28 17:56:08 +0100

    add - basic structure and website outline // add logos

---
+++ components/scripts/components.js
@@ -0,0 +1,15 @@
+// Load Header and footer
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('./components/header.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+      });
+      
+    fetch('./components/footer.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+      });
+  });
